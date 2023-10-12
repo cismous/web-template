c215f749f48d44b0f9947eb29623f7a48c19d5cb
cismous <cismous@gmail.com>
2023-10-13 00:27:53 +0800

    update

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 
 切换到项目目录下，可以执行以下命令:
 
-## `pnpm install`
+## `bun install`
 
 先安装项目依赖
 
 ## 安装本地包
 
-由于 @skzm/web-common @skzm/web-store-common 是本地包，需要单独执行安装
+由于 @skzm/web-common 是本地包，需要单独执行安装
 
-`pnpm link --global @skzm/web-common`
-`pnpm link --global @skzm/web-store-common`
+`bun link @skzm/web-common`
--- src/app.tsx
+++ src/app.tsx
@@ -2,24 +2,16 @@ import "./app.css";
 import * as React from "react";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/lib/locale/zh_CN";
-import { AppCtxProvider, GoodsCtxProvider } from "@skzm/web-store-common";
 import { AppRouter } from "./router";
-import OrderListCtxProvider from "./context/OrderListCtx";
 import { ICurrentUserInfoData, UserCtxProvider } from "@skzm/web-common";
 
 export default function App(props: { userInfo: ICurrentUserInfoData }) {
   return (
     <ConfigProvider locale={zhCN}>
       <UserCtxProvider userInfo={props.userInfo}>
-        <AppCtxProvider>
-          <GoodsCtxProvider>
-            <OrderListCtxProvider>
-              <React.StrictMode>
-                <AppRouter />
-              </React.StrictMode>
-            </OrderListCtxProvider>
-          </GoodsCtxProvider>
-        </AppCtxProvider>
+        <React.StrictMode>
+          <AppRouter />
+        </React.StrictMode>
       </UserCtxProvider>
     </ConfigProvider>
   );
